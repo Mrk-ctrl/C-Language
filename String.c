f41e84b9d7efe4a4c5b167246729d135f816f851
Mrk <123792284+Mrk-ctrl@users.noreply.github.com>
2024-11-19 19:51:14 +0530

    Add files via upload

--- String.c
+++ String.c
@@ -7,8 +7,37 @@ int main(){
     // printf("%s\n", arr);
     // printf("%c", arr[2]);
 
-    char str[] = "Hello";
-    printf("%s", str);
+    // char str[15] = "Hello";
+    // printf("%s", str);
+    // printf("\0");
+
+    // char ch = 'H'; //! Every Character represent an Integer constant value (ASCII)
+    // char str[] = "H"; //! String returns pointer to the first character
+
+    // int a = 10;
+    // printf(a, "%d");
+
+    // char str[] = "Hello";
+    // int a = 10;
+    // printf(str, "%s");
+    // printf(a);
+
+    // int a = 'H';
+    // printf("%d", a);
+
+    // char str[] = "Hello";
+    // printf("%s", str);
+    // puts(str);
+    // puts(str);
+
+    // char str[10];
+    // printf("Enter the string: ");
+    // scanf("%s", str);
+    // gets(str);
+    // fgets(str, sizeof(str), stdin);
+
+    // printf("%s", str);
+    // printf("%s", str);
 
     return 0;
 }
