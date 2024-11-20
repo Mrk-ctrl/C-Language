f7638264df19188475fb2995f75e65a6201ee548
Mrk <123792284+Mrk-ctrl@users.noreply.github.com>
2024-11-20 18:50:19 +0530

    Add files via upload

---
+++ Lambda_function.py
@@ -0,0 +1,9 @@
+# def sqr(a):
+#     return a * a
+
+num = int(input("Enter any Number: "))
+num2 = int(input("Enter any Number: "))
+
+n = lambda x, y: x + y
+
+print(f"The value is: {n(num, num2)}")
---
+++ set.py
@@ -0,0 +1,14 @@
+# s1 = {3, 56, 3, 56, 32, 90}
+# print(type(s1))
+# print(s1)
+
+# s1 = {54, 3, 6, 3, 90}
+# s2 = {7, 3, 67, 34, 23, 90, 5}
+
+# print(s1.union(s2))
+# s1.update(s2)
+# print(s1)
+
+# print(s1.intersection(s2))
+# s1.intersection_update(s2)
+# print(s1)
