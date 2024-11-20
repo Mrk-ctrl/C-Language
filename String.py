5ed76da0988149fd9532ef3cedb25aab0c008123
Mrk <123792284+Mrk-ctrl@users.noreply.github.com>
2024-11-20 18:49:24 +0530

    Add files via upload

---
+++ String.py
@@ -0,0 +1,47 @@
+# name = 'John'
+# print(name)
+# print(type(name))
+# print(name[2])
+
+# for i in name:
+#     print(i)
+
+# for i in range(0, len(name)):
+#     print(name[i])
+
+string = "!!!Hello World!!!"
+# print(string.index('o'))
+# print(string.count('o'))
+# print(string.upper())
+# print(string.lower())
+# print(string.isupper())
+# print(string.islower())
+# print(string.isalnum())
+# print(string.isalpha())
+# print(string.isascii())
+# print(string.isdecimal())
+# print(string.isdigit())
+# print(string.capitalize())
+# print(string.istitle())
+# print(string.title())
+# print(string.isspace())
+# print(string.endswith('d'))
+# print(string.startswith('h'))
+# print(string.center(20))
+# print(string.casefold())
+# print(string.expandtabs())
+# print(string.isprintable())
+# print(string.format())
+# name = "John"
+# age = 25
+# formatted_string = "My name is {} and I am {} years old.".format(name, age)
+# print(formatted_string)
+# print(string.find('W'))
+# print(string.isnumeric())
+# print(string.split())
+# print(string.replace("H", "Je"))
+# print(string.join("Good"))
+# print(string + "Good")
+# print(string.strip('!'))
+# print(string.lstrip('!'))
+# print(string.rstrip('!'))
